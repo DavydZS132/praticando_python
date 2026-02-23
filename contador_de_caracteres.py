f6c7450eb947a0acd4878e8012041025012602e8
Davyd Aparecido Zuquim Silva <156708495+DavydZS132@users.noreply.github.com>
2026-02-23 15:59:58 +0000

    Contador de Caraceteres

---
+++ contador_de_caracteres.py
@@ -0,0 +1,15 @@
+# def contador_de_caracteres(contador):
+#     return len(contador)
+
+# nome_caracteres = input('Digite uma palavra: ')
+
+# contador_caracteres = contador_de_caracteres(nome_caracteres)
+
+# print(f'Essa palavra tem {contador_caracteres} caracteres. ')
+
+
+def contar_caracteres(palavra): 
+    return len(palavra) 
+ 
+texto = input("Digite uma palavra: ") 
+print(f"Essa palavra tem {contar_caracteres(texto)} caracteres.") 
